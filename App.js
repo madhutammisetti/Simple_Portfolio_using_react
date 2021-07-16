c8eb6b193300dd21b6657152b98b5732afe84cd0
madhutammisetti <85747431+madhutammisetti@users.noreply.github.com>
2021-07-16 12:59:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./style.css"
+import Button from "./Comp.js"
+const  Madhuportfoilo=()=>{
+    return(
+        <div style={{textAlign: "center"}}>
+            <h1 className="name">Madhu Tammisetti</h1>
+            <pre className="hobbies">WebDeveloper DigitalMarketer VideoEditor </pre>
+            <p className="about">I have completed my bachelor's degree from R.V.R& J.C in 2020. I am Always ready to learn new technologies regarding 
+            web development and digital marketing.Seeking a beginner role to enhance and explore my technical knowledge about Front-end web 
+            development .*I also started creating projects with the technologies HTML , CSS, JAVASCRIPT.* I started my digital marketing carrier 
+            in 2020 from the company RK Technologies ,where we work on website designing using WordPress,* SEO and Email Marketing.* Also , worked with Sutherland as Customer Associate 
+            in february2020 where we worked for the US based banking sector "Green Dot".* I love Creating Logos and Posts and Editing the Videos
+            * Participating different coding Challenges in free time. </p>
+            <Button buttonText="LinkedIn" />
+            <Button buttonText="Github" />
+            <Button buttonText="Facebook" />
+            </div>
+    )
+}
+export default Madhuportfoilo
---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Madhuportfoilo from "./App.js"
+
+ReactDOM.render(<Madhuportfoilo/> ,document.getElementById("root"))
+
+
+
+
+
+
+ 
+
+
